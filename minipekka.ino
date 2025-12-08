55ecadf405aa705463be87384e5dd61a7f1efc4a
zio_serpente <moonezzaschifosa@gmail.com>
2025-12-08 20:06:07 +0100

    Il robot giustamente

--- minipekka.ino
+++ minipekka.ino
@@ -1,11 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
-
 #include <MeOrion.h>
-
-double angle_rad = PI/180.0;
-double angle_deg = 180.0/PI;
 MeDCMotor motor_9(9);//motore di destra
 MeDCMotor motor_10(10);//motore di sinistra
 MeDCMotor motor_1(1);//motore stringimento
@@ -13,25 +9,32 @@ MeDCMotor motor_2(2);//motore alzata
 MeUltrasonicSensor ultrasonic_8(8); //sensore ultrasuoni
 
 void setup(){
+    int robapresa=0;
     motor_1.run(100);
     motor_2.run(100);
     _delay(0.75);
-    
+    motor_1.run(0);
+    motor_2.run(0);
+    motor_9.run(200);
+    motor_10.run(200);
+    while(robapresa==0){
     if((ultrasonic_8.distanceCm()) > (0)){
         motor_9.run(0);
         motor_10.run(0);
         motor_1.run(0);
         motor_2.run(0);
+        robapresa=1
+    }
     }
 }
-//shai
+
 void loop(){
     _loop();
 }
 
-void _delay(float seconds){
-    long endTime = millis() + seconds * 1000;
-    while(millis() < endTime)_loop();
+void _delay(float s){
+    long fine = millis() + s * 1000;
+    while(millis() < fine)_loop();
 }
 
 void _loop(){
