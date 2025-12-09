bf122cd9dde9ffb0b59dcab22c7a4d3cc65da684
zio_serpente <moonezzaschifosa@gmail.com>
2025-12-09 17:58:12 +0100

    percorso

--- minipekka.ino
+++ minipekka.ino
@@ -7,12 +7,13 @@ MeDCMotor motor_10(10);//motore di sinistra
 MeDCMotor motor_1(1);//motore stringimento (potenza negativa->allarga, potenza positiva-> stringe)
 MeDCMotor motor_2(2);//motore alzata (potenza negativa->si abbassa, potenza positiva-> si alza)
 MeUltrasonicSensor ultrasonic_8(8); //sensore ultrasuoni, per rilevare gli ostacoli
+MeLineFollower linefollower_6(6);   //sensore per seguire il percorso dato (se )
 
 void setup(){
     int robapresa=0;
     resetbraccio();
-    motor_9.run(150);
-    motor_10.run(150);
+    motor_9.run(120);
+    motor_10.run(120);
     while(robapresa==0){
     if((ultrasonic_8.distanceCm()) < (16)){
         motor_9.run(0);
@@ -23,9 +24,35 @@ void setup(){
         motor_1.run(0);
         motor_2.run(0);
         robapresa=1;
-    }
+    }}
+    motor_9.run(150);
+    // motor_10.run(-100);
+    _delay(0.75);
+    motor_10.run(150);
+    while (robapresa==1)
+    {
+        switch (linefollower_6.readSensors())
+        {
+        case 3://lato destro e sinistro del sensore sono su colore bianco, il robot si ferma
+            motor_9.run(0);
+            motor_10.run(0);
+            break;
+        case 2: //girare a destra
+            motor_9.run(0);
+            motor_10.run(80);
+            break;
+        case 1: //girare a sinistra
+            motor_9.run(80);
+            motor_10.run(0);
+            break;
+        default: //in qualsiasi altro caso il robot va avanti
+            motor_9.run(120);
+            motor_10.run(120);
+            break;
+        }
     }
 }
+
 void resetbraccio(){
     motor_1.run(-120);
     motor_2.run(100);
@@ -33,14 +60,11 @@ void resetbraccio(){
     motor_1.run(0);
     motor_2.run(0);
 }
-void loop(){
-    _loop();
-}
+void loop(){_loop();}
 
 void _delay(float s){
     long fine = millis() + s * 1000;
     while(millis() < fine)_loop();
 }
 
-void _loop(){
-}
+void _loop(){}
